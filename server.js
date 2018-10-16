7350d8e8d0b8e44771ca399d73f915798a4be8c2
CSowers <charliesow@gmail.com>
2018-10-16 00:49:51 -0400

    heroku ready

--- server.js
+++ server.js
@@ -36,7 +36,9 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/abridge", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
